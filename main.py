d7aa87466db00495adf4ec98c0c32d8accd62044
Deirdre Cleary <deirdre@evervault.com>
2022-12-13 10:23:47 +0000

    Linting

--- main.py
+++ main.py
@@ -4,7 +4,7 @@
 def handler(data, context):
     # Check if the data sent into the Function included the `name` key
     if (data["name"] and isinstance(data["name"], str)):
-        print(f'A name of length {len(data["name"])} has arrived into the Function.');
+        print(f'A name of length {len(data["name"])} has arrived into the Function.')
 
 
         # Process the decrypted name value, and re-encrypt the original name using the encrypt function available in the context parameter.
@@ -13,7 +13,7 @@ def handler(data, context):
             "name": f'{context["encrypt"](data["name"])}',
         }
     else:
-        print('An empty name has arrived into the Function.');
+        print('An empty name has arrived into the Function.')
 
         return {
             "message": 'Hello from a Function! Send an encrypted `name` parameter to show Function decryption in action',
