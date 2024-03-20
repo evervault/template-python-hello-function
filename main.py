ab0e25ef09bea16acafead597938f7925ea23e62
Cian Butler <butlerx@notthe.cloud>
2024-03-20 10:53:43 +0000

    Check if the key name is defined in the data rather then if it is none

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
-# `data` is the data you encrypted and passed into `evervault.run` from your server. The Function 
-# automatically decrypts the data and maintains its structure so you can treat event exactly as 
-# you did when you passed it into `evervault.run`.
-def handler(data, context):
-    # Check if the data sent into the Function included the `name` key
-    if (data["name"] and isinstance(data["name"], str)):
-        print(f'A name of length {len(data["name"])} has arrived into the Function.')
+from typing import Any, Dict
 
+
+# `data` is the data you encrypted and passed into `evervault.run` from your server. The Function
+# automatically decrypts the data and maintains its structure so you can treat event exactly as
+# you did when you passed it into `evervault.run`.
+def handler(data: Any, context) -> Dict[str, str]:
+    # Check if the data sent into the Function included the `name` key
+    if "name" in data and isinstance(data["name"], str):
+        print(f'A name of length {len(data["name"])} has arrived into the Function.')
 
         # Process the decrypted name value, and re-encrypt the original name using the encrypt function available in the context parameter.
         return {
@@ -13,8 +15,8 @@ def handler(data, context):
             "name": f'{context["encrypt"](data["name"])}',
         }
     else:
-        print('An empty name has arrived into the Function.')
+        print("An empty name has arrived into the Function.")
 
         return {
-            "message": 'Hello from a Function! Send an encrypted `name` parameter to show Function decryption in action',
+            "message": "Hello from a Function! Send an encrypted `name` parameter to show Function decryption in action",
         }
